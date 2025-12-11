d4d75f47e4032af179ebb2b909025085f1781a1f
cparrasaldarriaga <saldaclau2017@gmail.com>
2025-12-11 17:50:32 -0500

    cp MetodoInsertarRadicado Se crea procedimiento almacenado para insertar un registro en la tabla cuopatra

--- CuotasPartesIPU/CuoparConDB.cs
+++ CuotasPartesIPU/CuoparConDB.cs
@@ -41,5 +41,52 @@ namespace CuotasPartesIPU
             return dt;
         }
 
+        public string InsertarRadicadoEnCupatra(string TipoDoco, string Documento, string Nombre, string Apellido, 
+            string Correo, string TipoDocumento, string Direccion, string Departamento, string Pais, string Descripcion,
+            string Apoderado, string CertificadoLibertad, string Poder, string Ususario, string Estado)
+        { 
+            string strConeccion = ConfigurationManager.ConnectionStrings["cuotaspartes_ConnectionString"].ConnectionString;
+            DataTable dt = new DataTable();
+
+            using (SqlConnection sqlconeccion = new SqlConnection(strConeccion))
+            {
+                try
+                {
+                    using (SqlCommand cmde = new SqlCommand("PA_INSERTAR_RADICADO_CUPATRA", sqlconeccion))
+                    {
+                        cmde.CommandType = CommandType.StoredProcedure;
+                        cmde.Parameters.AddWithValue("@P_CupTipDoc", TipoDoco);
+                        cmde.Parameters.AddWithValue("@P_CupDoc", Documento);
+                        cmde.Parameters.AddWithValue("@P_CupNom", Nombre);
+                        cmde.Parameters.AddWithValue("P_CupApe", Apellido);
+                        cmde.Parameters.AddWithValue("@P_CupCorreo", Correo);
+                        cmde.Parameters.AddWithValue("@P_CupTipoPet", TipoDocumento);       
+                        cmde.Parameters.AddWithValue("@P_CupDir", Direccion);
+                        cmde.Parameters.AddWithValue("@P_CupDep", Departamento);
+                        cmde.Parameters.AddWithValue("@P_CupPais", Pais);
+                        cmde.Parameters.AddWithValue("@P_CupDesc", Descripcion);
+                        cmde.Parameters.AddWithValue("@P_CupDocApo", Apoderado);
+                        cmde.Parameters.AddWithValue("@P_CupLyT", CertificadoLibertad);
+                        cmde.Parameters.AddWithValue("@P_CupPoder", Poder);
+                        cmde.Parameters.AddWithValue("@P_CupIdUsu", Ususario);
+                        cmde.Parameters.AddWithValue("@P_CupEsta", Estado);
+
+                        using (SqlDataAdapter da = new SqlDataAdapter(cmde))
+                        {
+                            da.Fill(dt);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    dt = new DataTable("Error");
+                    dt.Columns.Add("MensajeError", typeof(string));
+                    dt.Rows.Add("Error al consultar cargues de archivos ESSA: " + ex.Message);
+                }
+            }
+
+            return "";
+        }
+
     }
 }
